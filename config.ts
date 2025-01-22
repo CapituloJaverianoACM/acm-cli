ada5aa672bf2c18fb7f1057e106040429ff256c3
TalkySafe143 <sebastian.galindo143@gmail.com>
2025-01-21 23:21:07 -0500

    Updating the API URL

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ import Conf from "conf";
 
 const Config = new Conf( { projectName: 'acm-cli' });
 
-const API_URL = "http://localhost:3000";
+const API_URL = "https://acm-api-ysx5.onrender.com";
 Config.set('API_URL', API_URL);
 
 export default Config;
