6f7d9c79c397dbe4e680662786cd538929b2e12b
TalkySafe143 <sebastian.galindo143@gmail.com>
2025-06-21 19:38:10 -0500

    Feature: Members schema added

--- schemas/schemas.ts
+++ schemas/schemas.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod';
+import { z, ZodTypeAny } from 'zod';
 
 const activity = z.object({
         title: z.string(),
@@ -11,6 +11,35 @@ const activity = z.object({
         timestamp: z.string()
     }
 );
+const arrayFromString = <T extends ZodTypeAny>(schema: T) => {
+  return z.preprocess((obj: unknown) => {
+    if (!obj) {
+      return [];
+    }
+    if (Array.isArray(obj)) {
+      return obj;
+    }
+    if (typeof obj === 'string') {
+      return obj.split(',').map(_ => _.trim());
+    }
+
+    return [];
+  }, z.array(schema));
+};
+
+const members = z.object({
+  _id: z.coerce.number(),
+  name: z.string(),
+  career: z.string(),
+  rol: z.string(),
+  email: z.string().email(),
+  bio: z.string(),
+  skills: arrayFromString(z.string()),
+  image: z.string().optional(),
+  active: z.coerce.boolean()
+});
+
 export default {
-    activity
+    activity,
+    members
 }; 
